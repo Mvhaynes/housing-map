3a9dd6e2a2fca0c85cbd0bc5da254128bb80f6e2
melissa <melissahaynes@Melissas-Air-2.lan>
2021-04-28 20:19:37 -0500

    map

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// Create our initial map object
+// base map
 var dallas_coordinates = [32.7767, -96.7970]
 var map = L.map("map", {
   center: dallas_coordinates,
   zoom: 13
 });
 
-// Add a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
+// background map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
