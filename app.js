d21f72decb29afe6368315491f79e0e346aa3565
melissa <melissahaynes@Melissas-Air-2.lan>
2021-04-28 20:26:21 -0500

    updated

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// base map
 var dallas_coordinates = [32.7767, -96.7970]
-var map = L.map("map", {
-  center: dallas_coordinates,
-  zoom: 13
+var myMap = L.map("map", {
+  center: [32.7767, -96.7970],
+  zoom: 12
 });
 
-// background map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -13,5 +11,5 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   zoomOffset: -1,
   id: "mapbox/streets-v11",
   accessToken: api_key
-}).addTo(map);
+}).addTo(myMap);
 
