c6d5c9e2ba9f1e41895780e3c44203b42434b162
melissa <melissahaynes@Melissas-Air-2.lan>
2021-05-02 21:07:35 -0500

    avg calculations

--- static/js/app.js
+++ static/js/app.js
@@ -41,12 +41,35 @@ function getColor(price) { // Change numbers later
                   colorList[0]
 };
 
+var colorList = ['#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58']
+function colorMap(avg) {  
+  return avg > 4000000 ? colorList[8] :
+    avg > 1500000 ? colorList[7] :
+      avg > 850000 ? colorList[6] :
+        avg > 650000 ? colorList[5] :
+          avg > 550000 ? colorList[4] :
+            avg > 350000 ? colorList[3] :
+              avg > 250000 ? colorList[2] :
+                avg > 150000 ? colorList[1] :
+                  colorList[0],
+  console.log( avg > 4000000 ? colorList[8] : // to check function 
+    avg > 1500000 ? colorList[7] :
+      avg > 850000 ? colorList[6] :
+        avg > 650000 ? colorList[5] :
+          avg > 550000 ? colorList[4] :
+            avg > 350000 ? colorList[3] :
+              avg > 250000 ? colorList[2] :
+                avg > 150000 ? colorList[1] :
+                  colorList[0]
+  )};
+
+
 // Info box function  
 function popups(feature, layer) {
   layer.bindPopup(
     "<h3>" + feature.properties.name + "</h3><hr>" +
     "<p>Median Income: " +
-    "<p>Median House Price: "
+    "<p>Average House Price: "
   )
 };
 
@@ -63,23 +86,40 @@ function housePopup(feature, coordinate) {
 
 // Neighborhood outline  
 d3.json(outlines).then(function (data) {
+  
+  var features = data.features;
+  
+  function style(feature) {
+    var fill = {
+      fillColor: returnColor(feature.properties.name),
+      weight: 2,
+      opacity: 1,
+      color: 'black',
+      dashArray: '3',
+      fillOpacity: 0.7
+    }
+  }
 
   // create neighborhood outlines
-  L.geoJson(data, {
-    style: {
+  L.geoJson(data.features, {
+    
+    onEachFeature: function (feature, coordinates) {
+      var style = {
       "color": "black",
-      "fillColor": 'white',
+      "fillColor": returnColor(feature.properties.name),
       "opacity": 1
+      }
     },
-
     // Add pop up boxes 
-    onEachFeature: popups
-  }).addTo(myMap);
+    // onEachFeature: popups,
+    // onEachFeature: style
+    // })
+  }).addTo(myMap)
 });
 
-function markerSize(size) { // might not use 
-  return size / 200;
-}
+// function markerSize(size) { // might not use 
+//   return size / 200;
+// }
 
 // Plot houses
 d3.json(housePrices).then(function (data) {
@@ -107,8 +147,49 @@ d3.json(housePrices).then(function (data) {
 });
 
 
-// Loop through dallas neighborhoods and add median price ? idk
-function calculateMedian(neighborhood) {};
+
+ 
+
+function calculateAvg(place) {
+  d3.json('static/data/joined.json').then(function(data) {
+    var neighborhoodList = [];
+    var neighborhoodAvg = [];
+
+    // Loop through dallas neighborhoods and calculate average house price 
+    data.features.forEach(feature => {
+      var neighborhood = feature.properties.name;
+      
+      if (neighborhood in neighborhoodList) {
+        neighborhoodList[neighborhood] += 1;
+        neighborhoodList[neighborhood + ' price'] = neighborhoodList[neighborhood + ' price'] + (feature.properties.price);
+        neighborhoodAvg[neighborhood] = neighborhoodList[neighborhood + ' price'] / neighborhoodList[neighborhood];
+      }
+      else {
+        neighborhoodList[neighborhood] = 1;
+        neighborhoodList[neighborhood + ' price'] = (feature.properties.price);
+        neighborhoodAvg[neighborhood] = neighborhoodList[neighborhood + ' price']
+      };
+    })
+
+    // Fill in empty data (Do an auto loop later)
+    neighborhoodAvg['Design District'] = 0;
+    neighborhoodAvg['University Park'] = 0;
+    neighborhoodAvg['Knox'] = 0;
+    neighborhoodAvg['South Dallas'] = 0;
+    
+    var avg = neighborhoodAvg[place];
+    
+    console.log(avg)
+    
+    colorMap(avg)
+  })
+};
+
+
+function returnColor(place) { // delete later this is just to check the function 
+  calculateAvg(place);
+  console.log(place, calculateAvg(place)) // check 
+}
 
 
 // Police report heat layer 
